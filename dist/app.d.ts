2711f813fff370795a85c4439b790640cbaa7f7d
Brais <braiscao.dev@gmail.com>
2020-12-21 11:04:11 +0100

    2.8 - Object types

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,5 +1,4 @@
-declare function add(n1: number, n2: number, showResult: boolean, phrase: string): number;
-declare const number1 = 5;
-declare const number2 = 2.8;
-declare const printResult = true;
-declare const resultPhrase = "The result is: ";
+declare const person: {
+    name: string;
+    age: number;
+};
---
+++ dist/basics.d.ts
@@ -0,0 +1,5 @@
+declare function add(n1: number, n2: number, showResult: boolean, phrase: string): number;
+declare const number1 = 5;
+declare const number2 = 2.8;
+declare const printResult = true;
+declare const resultPhrase = "The result is: ";
