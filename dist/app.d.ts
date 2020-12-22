d2285b934269271bcc2c480112da0aa1fd667eba
Brais <braiscao.dev@gmail.com>
2020-12-22 09:36:36 +0100

    2.15 - Literal types

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,3 +1,4 @@
-declare function combine(input1: number | string, input2: number | string): any;
+declare function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text'): any;
 declare const combinedAges: any;
+declare const combinedStringAges: any;
 declare const combinedNames: any;
