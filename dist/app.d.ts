aeb2b15b7b07b4d20d620ae1261b9481bb6de29f
Brais <braiscao.dev@gmail.com>
2020-12-22 09:44:13 +0100

    2.16 - Types aliases, custom types

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,4 +1,6 @@
-declare function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text'): any;
+declare type Combinable = number | string;
+declare type Conversion = 'as-number' | 'as-text';
+declare function combine(input1: Combinable, input2: Combinable, resultConversion: Conversion): any;
 declare const combinedAges: any;
 declare const combinedStringAges: any;
 declare const combinedNames: any;
