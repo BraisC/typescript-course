7583b981e310d1c0c0159208d359feb2a3349717
Brais <braiscao.dev@gmail.com>
2021-02-16 12:07:07 +0100

    4.1 - Module introction

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,2 @@
-let userInput;
-let userName;
-userInput = 'Max';
-userInput = 6;
-console.log(userInput);
-// userName = userInput; // Esto no daría error si userInput fuese de tipo any
-console.log(userName);
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-console.log(userInput);
-// never produces a value, not even undefined
-function generateError(message, code) {
-    throw { message, errorCode: code };
-}
-generateError('Error', 500);
+// Code goes here
 //# sourceMappingURL=app.js.map
--- dist/union-aliases.d.ts
+++
@@ -1,6 +0,0 @@
-declare type Combinable = number | string;
-declare type Conversion = 'as-number' | 'as-text';
-declare function combine(input1: Combinable, input2: Combinable, resultConversion: Conversion): any;
-declare const combinedAges: any;
-declare const combinedStringAges: any;
-declare const combinedNames: any;
