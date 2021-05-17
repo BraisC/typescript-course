8e93f07e707b10ebee79b5531cbfa89b814fb362
Brais <braiscao.dev@gmail.com>
2021-05-17 19:48:33 +0200

    7.2 - Built nin generics & what are generics

--- dist/app.js
+++ dist/app.js
@@ -1,59 +1,9 @@
-const e1 = {
-    name: 'Brais',
-    privileges: ['create-server'],
-    startDate: new Date(),
-};
-console.log(e1);
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-const x = add(1, 2);
-function printEmployeeInformation(emp) {
-    console.log(`Name: ${emp.name}`);
-    if ('privileges' in emp) {
-        console.log(`Privileges: ${emp.privileges}`);
-    }
-}
-class Car {
-    drive() {
-        console.log('Driving...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('Driving a truck...');
-    }
-    loadCargo(amount) {
-        console.log(`Loading cargo ... ${amount}`);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log(`Moving with speed ${speed}`);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 30 });
-const input = document.getElementById('user-input');
-const input2 = document.getElementById('user-input');
-input.value = 'hi';
+const names = []; // Same as string[]
+names[0].split(' ');
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is done!');
+    }, 2000);
+});
+promise.then((data) => data.split(' '));
 //# sourceMappingURL=app.js.map
