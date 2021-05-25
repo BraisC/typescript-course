3706234c786fdbdd33f4f732d368d3b083aac6dd
Brais <braiscao.dev@gmail.com>
2021-05-25 21:04:28 +0200

    8.4 - Building more useful decorators

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,26 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/ban-types */
+// function Logger(logString: string) {
+//   return (constructor: Function) => {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-/* eslint-disable @typescript-eslint/ban-types */
-function Logger(logString) {
+function WithTemplate(template, hookId) {
     return (constructor) => {
-        console.log(logString);
-        console.log(constructor);
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        hookEl && (hookEl.innerHTML = template);
+        hookEl && (hookEl.querySelector('h1').textContent = p.name);
     };
 }
+// @Logger('Logging - Person')
 let Person = class Person {
     constructor() {
         this.name = 'Max';
@@ -18,7 +28,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger('Logging - Person')
+    WithTemplate('<h1>My person object</h1>', 'app')
 ], Person);
 const pers = new Person();
 console.log(pers);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/ban-types */
-function Logger(logString: string) {
-  return (constructor: Function) => {
-    console.log(logString);
-    console.log(constructor);
+// function Logger(logString: string) {
+//   return (constructor: Function) => {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+
+function WithTemplate(template: string, hookId: string) {
+  return (constructor: any) => {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    hookEl && (hookEl.innerHTML = template);
+    hookEl && (hookEl.querySelector('h1')!.textContent = p.name);
   };
 }
 
-@Logger('Logging - Person')
+// @Logger('Logging - Person')
+@WithTemplate('<h1>My person object</h1>', 'app')
 class Person {
   name: string;
 
