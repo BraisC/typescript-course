f94df1d2332d6451863ec77cd71a7107d43a62a8
Brais Cao <brais.cao@xing.com>
2022-01-15 15:48:35 +0100

    14.8 - Managing State Better

--- s14_react_and_ts/src/App.tsx
+++ s14_react_and_ts/src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
   const [todos, setTodos] = useState<Array<Todo>>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text }]);
+    setTodos((prevTodos) => [...prevTodos, { id: Math.random().toString(), text }]);
   };
 
   return (
